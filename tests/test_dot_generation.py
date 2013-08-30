962cdb01135d262e13b708ed508b8368c57c7c6b
Rafael Carrascosa <rcarrascosa@machinalis.com>
2013-08-30 15:41:14 -0300

    Splitted test filed into one per generation type: sparql and dot.

--- tests/test_dot_generation.py
+++ tests/test_dot_generation.py
@@ -8,6 +8,10 @@
 #          Gonzalo Garcia Berrotaran <ggarcia@machinalis.com>
 
 import unittest
+import tempfile
+import subprocess
+from random_expression import random_expression
+from random import seed
 from quepy.generation import get_code
 from quepy.dsl import FixedRelation, FixedType, \
     FixedDataRelation
@@ -31,7 +35,7 @@ def gen_fixedrelation(rel, e):
     return X(e)
 
 
-class TestCodeGeneration(unittest.TestCase):
+class TestSparqlGeneration(unittest.TestCase):
 
     def _standard_check(self, s, e):
         self.assertIsInstance(s, unicode)
@@ -56,22 +60,27 @@ class TestCodeGeneration(unittest.TestCase):
         e += gen_datarel("tµŧurułej€", "←ðßðæßđæßæđßŋŋæ @~~·ŋŋ·¶·ŋ“¶¬@@")
         self.assertRaises(ValueError, get_code, e, "dot")
 
-    def test_sparql_takes_unicode(self):
-        e = gen_fixedtype(u"·̣─@łæßð~¶½")
-        e += gen_datarel(u"tµŧurułej€", u"←ðßðæßđæßæđßŋŋæ @~~·ŋŋ·¶·ŋ“¶¬@@")
-        _, s = get_code(e, "sparql")
-        self._standard_check(s, e)
+    def test_dot_stress(self):
+        seed("I have come here to chew bubblegum and kick ass... "
+             "and I'm all out of bubblegum.")
+        dot_file = tempfile.NamedTemporaryFile()
+        cmdline = "dot -Tpng %s" % dot_file.name
+        msg = "dot returned error code {}, check {} input file."
+        for _ in xrange(100):
+            expression = random_expression()
+            _, dot_string = get_code(expression, "dot")
+            with open(dot_file.name, "w") as filehandler:
+                filehandler.write(dot_string.encode("utf-8"))
 
-    def test_sparql_takes_fails_ascii1(self):
-        e = gen_fixedtype("a")
-        e += gen_datarel("b", "c")
-        e = gen_fixedrelation("d", e)
-        self.assertRaises(ValueError, get_code, e, "sparql")
-
-    def test_sparql_takes_fails_ascii2(self):
-        e = gen_fixedtype("·̣─@łæßð~¶½")
-        e += gen_datarel("tµŧurułej€", "←ðßðæßđæßæđßŋŋæ @~~·ŋŋ·¶·ŋ“¶¬@@")
-        self.assertRaises(ValueError, get_code, e, "sparql")
+            try:
+                retcode = subprocess.call(cmdline.split(),
+                                          stdout=tempfile.TemporaryFile())
+            except OSError:
+                print "Warning: the program 'dot' was not found, tests skipped"
+                return
+            if retcode != 0:
+                dot_file.delete = False
+            self.assertEqual(retcode, 0, msg.format(retcode, dot_file.name))
 
 
 if __name__ == "__main__":
